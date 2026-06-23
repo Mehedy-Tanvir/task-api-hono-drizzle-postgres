bc5a8d222050a034f7fea6646d4b9e8c4a9a5180
Md. Mehedy Hasan Tanvir <mehedytanvir451@gmail.com>
2026-06-23 22:51:54 +0600

    feat: open api zod added and app instance separated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+import { createApp } from "./app";
 import env from "./env";
 
-const app = new Hono();
+const app = createApp();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
